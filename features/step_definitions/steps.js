9ef68c1a4b1a350a6e40742b7346b0a7a0415088
Antonio Kobashikawa <akobashikawa@gmail.com>
2024-08-12 11:40:01 -0500

    Implement steps

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,2 +1,17 @@
 const { Before, Given, When, Then } = require('@cucumber/cucumber');
 const { assertThat, is } = require('hamjest');
+
+function helloworld() {
+    return 'Hello World!';
+}
+
+Given('User is in console', function () {
+});
+
+When('User run the app', function () {
+});
+
+Then('{string} is displayed', function (message) {
+    const helloMessage = helloworld();
+    assertThat(helloMessage, is(message));
+});
