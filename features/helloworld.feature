2a13a7a179bc0117f19c7ea375321570b0a27cfe
Antonio Kobashikawa <akobashikawa@gmail.com>
2024-08-12 14:57:38 -0500

    Add scenarios

--- features/helloworld.feature
+++ features/helloworld.feature
@@ -4,7 +4,17 @@ Feature: Say Hello World
 
     Rule: HelloWorld always says same message
 
+        Scenario: User in services
+            Given User is in services
+            When User request service helloworld
+            Then "Hello World!" is returned
+
         Scenario: User in console
             Given User is in console
             When User run the app
-            Then "Hello World!" is displayed
+            Then "Hello World!" is displayed in console
+
+        Scenario: User in browser
+            Given User is in browser
+            When User open home page
+            Then "Hello World!" is displayed in browser
--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,17 +1,54 @@
 const { Before, Given, When, Then } = require('@cucumber/cucumber');
 const { assertThat, is } = require('hamjest');
+const sinon = require('sinon');
 
-function helloworld() {
+function helloworldService() {
     return 'Hello World!';
 }
 
+function helloworldConsole() {
+    const message = helloworldService();
+    console.log(message);
+}
+
+Given('User is in services', function () {
+    this.serviceResult = '';
+});
+
+When('User request service helloworld', function () {
+    this.serviceResult = helloworldService();
+});
+
+Then('{string} is returned', function (message) {
+    assertThat(this.serviceResult, is(message));
+});
+
+
 Given('User is in console', function () {
+    this.consoleSpy = sinon.spy(console, 'log');
 });
 
 When('User run the app', function () {
+    helloworldConsole();
 });
 
-Then('{string} is displayed', function (message) {
-    const helloMessage = helloworld();
-    assertThat(helloMessage, is(message));
+Then('{string} is displayed in console', function (message) {
+    assertThat(true, this.consoleSpy.calledWith(message));
+    this.consoleSpy.restore();
+});
+
+
+Given('User is in browser', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('User open home page', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('{string} is displayed in browser', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
 });
